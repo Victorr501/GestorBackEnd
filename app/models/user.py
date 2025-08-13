2950a608459f0f066b8c988d542519e9efe7e062
victor <vrubinr501@gmail.com>
2025-08-13 15:16:36 +0200

    13/08/2025

--- app/models/user.py
+++ app/models/user.py
@@ -5,5 +5,6 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    #Se especfica una longitud para el tipo de String en MySQL
+    email = Column(String(225), unique=True, index=True)
+    hashed_password = Column(String(225))
